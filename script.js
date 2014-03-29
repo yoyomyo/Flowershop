1134673ffa6b3c9a4267b139b8aaecc75d791df1
yoyomyo <yoyomyo>
2014-03-29 14:20:22 -0400

    adding functions to change graph dynamically

--- script.js
+++ script.js
@@ -11,14 +11,12 @@ var jsonData = [
 {"flower": "rose", "date": "2/5/2012", "quantity-sold": "55", "quantity-unsold": "35"},
 {"flower": "rose", "date": "2/6/2012", "quantity-sold": "70", "quantity-unsold": "20"},
 {"flower": "rose", "date": "2/7/2012", "quantity-sold": "30", "quantity-unsold": "70"},
-{"flower": "rose", "date": "2/8/2012", "quantity-sold": "40", "quantity-unsold": "60"},
 {"flower": "dandelion", "date": "2/3/2012", "quantity-sold": "10", "quantity-unsold": "0"},
 {"flower": "dandelion", "date": "2/4/2012", "quantity-sold": "9", "quantity-unsold": "11"},
 {"flower": "dandelion", "date": "2/5/2012", "quantity-sold": "3", "quantity-unsold": "17"},
 {"flower": "dandelion", "date": "2/6/2012", "quantity-sold": "4", "quantity-unsold": "16"},
 {"flower": "dandelion", "date": "2/7/2012", "quantity-sold": "7", "quantity-unsold": "8"}
 ]
-
 
 //checkbox values
 var sold = true
@@ -40,55 +38,65 @@ for(var d in jsonData){
     if(!(row.flower in flowers)){ flowers[row.flower] = []}
 
     if(sold && unsold){
-        flowers[row.flower].push({date:row.date, begin: 0, end:row.sold});
-        flowers[row.flower].push({date:row.date, begin: row.sold, end:row.sold + row.unsold});
+        flowers[row.flower].push({date:row.date, bottom:0, top:row.sold, sold:true});
+        flowers[row.flower].push({date:row.date, bottom:row.sold, top:row.sold+row.unsold, unsold:true});
     }else if(sold){
-        flowers[row.flower].push({date:row.date, sold:row.sold});
+        flowers[row.flower].push({date:row.date, bottom:0, top:row.sold, sold:true});
     }else if(unsold){
-        flowers[row.flower].push({date:row.date, unsold:row.unsold});
+        flowers[row.flower].push({date:row.date, bottom:0, top:row.unsold, unsold:true});
     }
 
 
     if(!(row.date in dates)){ dates[row.date] = [] }
 
     if(sold && unsold){
-        dates[row.date].push({flower:row.flower, bottom:0, top:row.sold});
-        dates[row.date].push({flower:row.flower, bottom:row.sold, top:row.sold+row.unsold});
+        dates[row.date].push({flower:row.flower, bottom:0, top:row.sold, sold:true});
+        dates[row.date].push({flower:row.flower, bottom:row.sold, top:row.sold+row.unsold, unsold:true});
     }else if(sold){
-        dates[row.date].push({flower:row.flower, sold:row.sold});
+        dates[row.date].push({flower:row.flower, bottom:0, top:row.sold, sold:true});
     }else if(unsold){
-        dates[row.date] = [{flower:row.flower, unsold:row.unsold}];
+        dates[row.date].push({flower:row.flower, bottom:0, top:row.unsold, unsold:true});
     }
 }
 
 
 
-var datesArray = []
+var allDates = []
 for (var key in dates) {
-    datesArray.push({date:key, value:dates[key]});
+    allDates.push({date:key, value:dates[key]});
 }
 
-var flowersArray = []
+var allFlowers = []
 for (var key in flowers) {
-    flowersArray.push({flower:key, value:flowers[key]});
+    allFlowers.push({flower:key, value:flowers[key]});
+}
+
+  var legendData = Object.keys(flowers).slice().map(function(d){return d+'(sold)'});
+  Object.keys(flowers).slice().map(function(d){ legendData.push(d+'(unsold)')})
+
+//user input
+var dateArray = ['2/3/2012', '2/4/2012']
+var flowerArray = ['tulip', 'dandelion']
+var sold = true
+var unsold = true
+
+
+//domain of x axis, dates
+//domain of xx axis, flowers
+//domain of y axis, sold, unsold or sold+unsold
+//domain of color, flower, either sold or unsold
+function getDataSubset(dateArray, flowerArray, sold, unsold){
+    for(var i in dateArray){
+        dates[dateArray[i]]
+    }
 }
 
 
-
-// var format = d3.time.format("%m/%d/%Y")
-// var amountFn = function(d) { return d.sold }
-// var dateFn = function(d) { return format.parse(d.date) }
-
-var width = 960,
-    height = 500;
-
-var barWidth = width / data.length;
 
 //set chart width and height
 var chart = d3.select(".chart")
     .attr("height", height)
     .attr("width", width);
-
 
 var margin = {top: 20, right: 30, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
@@ -96,29 +104,23 @@ var margin = {top: 20, right: 30, bottom: 30, left: 40},
 
 //param: array of dates, array of flowers, boolean sold, boolean unsold
 
-// // var x = d3.scale.ordinal()
-// //     .rangeRoundBands([0, width], .1);
-// var y = d3.scale.linear()
-//     .domain([0, d3.max(data, function(d) { return d.sold; })])
-//     .range([height, 0]);
-
 var x = d3.scale.ordinal()
     .domain(data.map(function(d) { return d.date; }))
-    .rangeBands([10, width]);
+    .rangeBands([10, width-100]);
 
 var xx = d3.scale.ordinal()
         .domain(data.map(function(d) { return d.flower; }))
-        .rangeRoundBands([10, x.rangeBand()]);
+        .rangeRoundBands([30, x.rangeBand()-30]);
 
-  // x0.domain(data.map(function(d) { return d.State; }));
-  //x1.domain(ageNames).rangeRoundBands([0, x0.rangeBand()]);
-var soldColor = d3.scale.ordinal()
-    .domain(data.map(function(d) { return d.flower; }))
-    .range(["#98abc5", "#8a89a6", "#7b6888"]);
+// var color = d3.scale.ordinal()
+//     .domain(legendData)
+//     .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
-var unsoldColor = d3.scale.ordinal()
-    .domain(data.map(function(d) { return d.flower; }))
-    .range(["#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+    var color = {};
+    colorCandidate = ["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"];
+    for(var i in legendData){
+        color[legendData[i]] = colorCandidate[i];
+    }
 
 var y = d3.scale.linear()
     .domain([0, d3.max(data, function(d) { return d.sold+d.unsold; })])
@@ -147,8 +149,8 @@ var chart = d3.select(".chart")
       .attr("class", "y axis")
       .call(yAxis);
 
-  var state = chart.selectAll(".dates")
-      .data(datesArray)
+  var group = chart.selectAll(".dates")
+      .data(allDates)
       .enter().append("g")
       .attr("class", "g")
       .attr("transform", function(d) { 
@@ -156,8 +158,8 @@ var chart = d3.select(".chart")
     });
  
 
-  if(sold && unsold){
-      var bar = state.selectAll("rect")
+  // if(sold && unsold){
+      var bar = group.selectAll("rect")
           .data(function(d) { return d.value })
           .enter().append("rect")
           .attr("width", xx.rangeBand())
@@ -165,28 +167,26 @@ var chart = d3.select(".chart")
           .attr("y", function(d) { return y(d.top); })
           .attr("height", function(d) { return y(d.bottom) - y(d.top); })
           .style("fill", function(d,i) { 
-                if(d.bottom ==0){
-                    console.log("sold " + d.flower + ":"+soldColor(d.flower))
-                    return soldColor(d.flower); 
+                if(d.hasOwnProperty('sold')){
+                    return color[d.flower+'(sold)']; 
                 }else{
-                    console.log("unsold " + d.flower + ":"+unsoldColor(d.flower))
-                    return unsoldColor(d.flower);  
+                    return color[d.flower+'(unsold)'];  
                 }               
         })    
-  }
+  // }
 
 
   var legend = chart.selectAll(".legend")
-      .data(Object.keys(flowers).slice().reverse())
-    .enter().append("g")
+      .data(legendData)
+      .enter().append("g")
       .attr("class", "legend")
       .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
   legend.append("rect")
-      .attr("x", width - 18)
+      .attr("x", width -24)
       .attr("width", 18)
       .attr("height", 18)
-      .style("fill", soldColor);
+      .style("fill", function(d) {console.log(d); return color[d]});
 
   legend.append("text")
       .attr("x", width - 24)
